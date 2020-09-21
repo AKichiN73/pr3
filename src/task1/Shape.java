545b1f949d6e593cac40070daf72b5ecd666a2c0
Saint <krogstat@gmail.com>
2020-09-21 13:10:30 +0300

    11

---
+++ src/task1/Shape.java
@@ -0,0 +1,8 @@
+package task1;
+import java.lang.*;
+public abstract class Shape {
+    protected String color;
+    protected boolean filled;
+    public abstract double getArea();
+    public abstract double getPerimeter();
+}
